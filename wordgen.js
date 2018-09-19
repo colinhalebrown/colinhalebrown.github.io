5913fb9595d286bfc07eba33aae79ce9c7472174
Colin Hale-Brown <colinhalebrown+GIT@gmail.com>
2018-09-18 17:35:06 -0700

    reset index

--- wordgen.js
+++
@@ -1,4 +0,0 @@
-var wordArray = ["one", "two", "three", "four", "five", "six", "seven"]; //No need for seven lines of push statements
-var randNum, finalWord;
-randNum = Math.floor(Math.random() * wordArray.length);
-finalWord = wordArray[randNum];
